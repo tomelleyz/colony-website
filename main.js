fa8b6401dcb49f03eafa39940da54d979ca0a30f
Ola Eleyinmi <eleyinmiolasunkanmi@gmail.com>
2018-11-06 17:49:42 +0100

    Added a page preloader and animated the body

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+var load;
+
+function Loader() {
+    load = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("page").style.display = "block";
+}
 function openMenu() {
     document.getElementById("sidenav").style.width = "250px";
 }
