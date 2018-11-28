dc12825aed4a82391e8b64c5115470b9b7baae98
Ola Eleyinmi <eleyinmiolasunkanmi@gmail.com>
2018-11-28 01:32:49 +0100

    Added the Colony blue and white logos, favicon and general documentation.

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-var load;
-
 function Loader() {
-    load = setTimeout(showPage, 3000);
+    setTimeout(showPage, 3000);
 }
 
 function showPage() {
@@ -9,9 +7,11 @@ function showPage() {
     document.getElementById("loaderLogo").style.display = "none";
     document.getElementById("page").style.display = "block";
 }
+
 function openMenu() {
     document.getElementById("sidenav").style.width = "60%";
 }
+
 function closeMenu() {
     document.getElementById("sidenav").style.width = "0";
 }
